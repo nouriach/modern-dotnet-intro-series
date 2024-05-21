ae54dd22aa3262b60672d8ebb7be788c1192fc14
nouriach <nouriach17@gmail.com>
2024-05-21 15:23:30 +0100

    02 Introduce Dependency Injection #2
    - Install .NET IoC package for Dependency Injection
    - Create new Interfaces for `BreweryService` and `ApiClient`
    - Register Services in `Program`
    - Inject interfaces into endpoints and `BreweryService`
    - Remove concrete implementations

--- src/BreweryApi.Endpoints/Program.cs
+++ src/BreweryApi.Endpoints/Program.cs
@@ -4,6 +4,9 @@ builder.Services.AddHealthChecks();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<IApiClient, BreweryApiClient>();
+builder.Services.AddSingleton<IBreweryService, BreweryService>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -13,26 +16,18 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHealthChecks("/health");
 
-var breweries = new List<Brewery>
+app.MapGet("/brewery", (IBreweryService breweryService) =>
 {
-    new Brewery { Id = Guid.Parse("5128df48-79fc-4f0f-8b52-d06be54d0cec"), Name = "(405) Brewing Co", City = "Norman", State = "Oklahoma", WebsiteUrl = "http://www.405brewing.com" },
-    new Brewery { Id = Guid.Parse("9c5a66c8-cc13-416f-a5d9-0a769c87d318"), Name = "(512) Brewing Co", City = "Austin", State = "Texas", WebsiteUrl = "http://www.512brewing.com" },
-    new Brewery { Id = Guid.Parse("34e8c68b-6146-453f-a4b9-1f6cd99a5ada"), Name = "1 of Us Brewing Company", City = "Mount Pleasant", State = "Wisconsin", WebsiteUrl = "https://www.1ofusbrewing.com" },
-    new Brewery { Id = Guid.Parse("08f78223-24f8-4b71-b381-ea19a5bd82df"), Name = "11 Below Brewing Company", City = "Houston", State = "Texas", WebsiteUrl = "http://www.11belowbrewing.com" },
-};
-
-app.MapGet("/brewery", () =>
-{
-    return new BreweryService().GetAllBreweries();
+    return breweryService.GetAllBreweries();
 });
 
-app.MapGet("/brewery/{id}", (string id) => {
+app.MapGet("/brewery/{id}", (IBreweryService breweryService, string id) => {
     Guid breweryId;
     var validId = Guid.TryParse(id, out breweryId);
     
     if (validId)
     {
-        var matchingBrewery = new BreweryService().GetBreweryById(breweryId);
+        var matchingBrewery = breweryService.GetBreweryById(breweryId);
         if (matchingBrewery == null)
             return Results.NotFound($"No brewery was found with the id: {id}");
 
@@ -42,20 +37,20 @@ app.MapGet("/brewery/{id}", (string id) => {
     return Results.BadRequest($"The Id you passed was not a Guid: {id}");
 });
 
-app.MapPost("/brewery", (Brewery brewery) =>
+app.MapPost("/brewery", (IBreweryService breweryService, Brewery brewery) =>
 {
-    var createdBrewery = new BreweryService().CreateBrewery(brewery);
+    var createdBrewery = breweryService.CreateBrewery(brewery);
     return Results.Ok(createdBrewery);
 });
 
-app.MapPut("/brewery/{id}", (string id, Brewery updatedBrewery) =>
+app.MapPut("/brewery/{id}", (IBreweryService breweryService, string id, Brewery updatedBrewery) =>
 {
     Guid breweryId;
     var validId = Guid.TryParse(id, out breweryId);
 
     if (validId)
     {
-        var matchingBrewery = new BreweryService().UpdateBrewery(breweryId, updatedBrewery);
+        var matchingBrewery = breweryService.UpdateBrewery(breweryId, updatedBrewery);
         if (matchingBrewery == null)
             return Results.NotFound($"No brewery was found with the id: {id}");
 
@@ -70,13 +65,13 @@ app.MapPut("/brewery/{id}", (string id, Brewery updatedBrewery) =>
     return Results.BadRequest($"The Id you passed was not a Guid: {id}");
 });
 
-app.MapDelete("/brewery/{id}", (string id) => {
+app.MapDelete("/brewery/{id}", (IBreweryService breweryService, string id) => {
     Guid breweryId;
     var validId = Guid.TryParse(id, out breweryId);
 
     if (validId)
     {
-        var remainingBreweries = new BreweryService().DeleteBrewery(breweryId);
+        var remainingBreweries = breweryService.DeleteBrewery(breweryId);
         if (remainingBreweries == null)
             return Results.NotFound("Sorry, this brewery does not exist.");
 
@@ -97,35 +92,40 @@ class Brewery
     public string WebsiteUrl { get; set; }
 }
 
-class BreweryService
+class BreweryService : IBreweryService
 {
+    private IApiClient _breweryClient;
+    public BreweryService(IApiClient breweryClient)
+    {
+        _breweryClient = breweryClient;
+    }
     public IEnumerable<Brewery> GetAllBreweries()
     {
-        return new BreweryApiClient().GetAllBreweries();
+        return _breweryClient.GetAllBreweries();
     }
 
     public Brewery  GetBreweryById(Guid breweryId)
     {
-        return new BreweryApiClient().GetBreweryById(breweryId);
+        return _breweryClient.GetBreweryById(breweryId);
     }
 
     public Brewery  CreateBrewery(Brewery brewery)
     {
-        return new BreweryApiClient().CreateBrewery(brewery);
+        return _breweryClient.CreateBrewery(brewery);
     }
 
     public Brewery UpdateBrewery(Guid id, Brewery brewery)
     {
-        return new BreweryApiClient().UpdateBrewery(id, brewery);
+        return _breweryClient.UpdateBrewery(id, brewery);
     }
 
     public IEnumerable<Brewery> DeleteBrewery(Guid id)
     {
-        return new BreweryApiClient().DeleteBrewery(id);
+        return _breweryClient.DeleteBrewery(id);
     }
 }
 
-class BreweryApiClient
+class BreweryApiClient : IApiClient
 {
     private List<Brewery> _breweries;
 
@@ -182,4 +182,22 @@ class BreweryApiClient
   
         return _breweries;
     }
+}
+
+interface IApiClient
+{
+    IEnumerable<Brewery> GetAllBreweries();
+    Brewery GetBreweryById(Guid id);
+    Brewery CreateBrewery(Brewery brewery);
+    Brewery UpdateBrewery(Guid id, Brewery brewery);
+    IEnumerable<Brewery> DeleteBrewery(Guid id);
+}
+
+interface IBreweryService
+{
+    IEnumerable<Brewery> GetAllBreweries();
+    Brewery GetBreweryById(Guid id);
+    Brewery CreateBrewery(Brewery brewery);
+    Brewery UpdateBrewery(Guid id, Brewery brewery);
+    IEnumerable<Brewery> DeleteBrewery(Guid id);
 }
